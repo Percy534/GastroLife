0a8e72355c11fe61ab6fe9aceb5c7bdde2b2fc17
@percybm43 <percybm43@gmail.com>
2022-11-29 22:35:56 -0600

    cambios

--- ingresar.php
+++ ingresar.php
@@ -1,3 +1,33 @@
+<?php
+
+    require 'db.php';
+
+        if($_POST){
+            //var_dump($_POST);
+            $user = $database-> select("tb_users","*",[
+                "user_name" => $_POST["username"]
+            ]);
+
+            if(count($user) > 0){
+                if(password_verify($_POST["password"], $user[0]["password"])){
+                    //echo "valid username and password";
+
+                    //init session
+                    session_start();
+                    $_SESSION["isLoggedIn"] = true;
+                    $_SESSION["username"] = $user[0]["user_name"];
+                    //redireccionar
+                    header("Location: recipes.php");
+
+                }else{
+                    echo "wrong username or password";
+                }
+            }else{
+                echo "wrong username or password";
+            }
+        }
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 
